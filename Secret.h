7f122b03bcf05ebd1ca9e99668c1dc35421eb81b
hyunwoo022 <127804391+hyunwoo022@users.noreply.github.com>
2023-12-05 16:40:27 +0900

    Create Secret.h

---
+++ Secret.h
@@ -0,0 +1,10 @@
+#include <pgmspace.h>
+
+#define WIFI_SSID "KT_GiGA_5G_B89C"
+#define WIFI PASSWORD "3cb30bf139"
+
+//FIREBASE PROJECT API KEY
+#define API_KEY "AIzaSyAW-wwzTjkngagcoakU2HFdaYwN0kBmN60"
+
+//RTDB URL
+#define DATABASE_URL "esp32-realy-default-rtdb.firebaseio.com/"
